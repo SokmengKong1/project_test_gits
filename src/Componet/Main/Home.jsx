78fb42d478786a163c048edddf680e00b67ed314
Hostka <150564821+Hostka@users.noreply.github.com>
2024-10-09 22:32:03 +0700

    first commit

---
+++ src/Componet/Main/Home.jsx
@@ -0,0 +1,268 @@
+
+'use client';
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Style.css'
+import './Style/Sryle.css'
+
+
+const Home = () => {
+    return (<>
+
+        <div className="All w-[100%] h-[600px] bg-slate-700  justify-center ">
+            <div className='container mx-auto x flex justify-center items-center main-grid'>
+                <div className="tatile ">
+                    <p className='text-red-700 font-bold '>Silver Aluminu</p>
+                    <p className='text-4xl font-bold'>
+                        APPLE WATCH
+                    </p>
+                    <p className=' text-white'>
+                        30% off at your first odder
+                    </p>
+                    <div className="flex w-36">
+                        <button type='button' className='h-9 w-40 rounded-md bg-yellow-500 '>Shop Now </button>
+
+                    </div>
+
+                </div>
+                <div className="items-center mt-16 flex justify-center ">
+                    <img className='h-96 ' src="https://www.casio.com/content/dam/casio/product-info/locales/intl/en/timepiece/product/watch/G/GA/GA2/ga-2100mng-2a/assets/GA-2100MNG-2A.png.transform/main-visual-sp/image.png" alt="" />
+                </div>
+
+            </div>
+
+        </div>
+        {/* Card */}
+        <div className=" grid grid-rows-2 grid-flow-col gap-4 pt-6 w-[100%]  ">
+            <div className=" grid grid-cols-5 gap-4 pl-24  ">
+                <div className=" w-64 bg-slate-950 rounded-md shadow-sm" >
+                    <img src="https://www.casio.com/content/dam/casio/product-info/locales/intl/en/timepiece/product/watch/M/MW/MWD/mwd-110h-8av/assets/MWD-110H-8AV.png.transform/product-panel/image.png" alt="" />
+                    <div className="pl-4">
+                        <span className='text-white'>PRODUCT!</span>
+                        <p className='text-red-500'>$29</p>
+                    </div>
+                    <div className=" pl-16 items-center pb-2">
+                        <button className='flex items-center justify-center h-8 bg-yellow-200 w-32 rounded-md'>Add To Card</button>
+
+                    </div>
+
+                </div>
+                <div className=" w-64 bg-slate-950 rounded-md shadow-sm" >
+                    <img src="https://www.casio.com/content/dam/casio/product-info/locales/intl/en/timepiece/product/watch/M/MW/MWD/mwd-110h-8av/assets/MWD-110H-8AV.png.transform/product-panel/image.png" alt="" />
+                    <div className="pl-4">
+                        <span className='text-white'>PRODUCT!</span>
+                        <p className='text-red-500'>$29</p>
+                    </div>
+                    <div className=" pl-16 items-center pb-2">
+                        <button className='flex items-center justify-center h-8 bg-yellow-200 w-32 rounded-md'>Add To Card</button>
+
+                    </div>
+
+                </div>
+                <div className=" w-64 bg-slate-950 rounded-md shadow-sm" >
+                    <img src="https://www.casio.com/content/dam/casio/product-info/locales/intl/en/timepiece/product/watch/M/MW/MWD/mwd-110h-8av/assets/MWD-110H-8AV.png.transform/product-panel/image.png" alt="" />
+                    <div className="pl-4">
+                        <span className='text-white'>PRODUCT!</span>
+                        <p className='text-red-500'>$29</p>
+                    </div>
+                    <div className=" pl-16 items-center pb-2">
+                        <button className='flex items-center justify-center h-8 bg-yellow-200 w-32 rounded-md'>Add To Card</button>
+
+                    </div>
+
+                </div>
+                <div className=" w-64 bg-slate-950 rounded-md shadow-sm" >
+                    <img src="https://www.casio.com/content/dam/casio/product-info/locales/intl/en/timepiece/product/watch/M/MW/MWD/mwd-110h-8av/assets/MWD-110H-8AV.png.transform/product-panel/image.png" alt="" />
+                    <div className="pl-4">
+                        <span className='text-white'>PRODUCT!</span>
+                        <p className='text-red-500'>$29</p>
+                    </div>
+                    <div className=" pl-16 items-center pb-2">
+                        <button className='flex items-center justify-center h-8 bg-yellow-200 w-32 rounded-md'>Add To Card</button>
+
+                    </div>
+
+                </div>
+                <div className=" w-64 bg-slate-950 rounded-md shadow-sm" >
+                    <img src="https://www.casio.com/content/dam/casio/product-info/locales/intl/en/timepiece/product/watch/M/MW/MWD/mwd-110h-8av/assets/MWD-110H-8AV.png.transform/product-panel/image.png" alt="" />
+                    <div className="pl-4">
+                        <span className='text-white'>PRODUCT!</span>
+                        <p className='text-red-500'>$29</p>
+                    </div>
+                    <div className=" pl-16 items-center pb-2">
+                        <button className='flex items-center justify-center h-8 bg-yellow-200 w-32 rounded-md'>Add To Card</button>
+
+                    </div>
+
+                </div>
+                <div className=" w-64 bg-slate-950 rounded-md shadow-sm" >
+                    <img src="https://www.casio.com/content/dam/casio/product-info/locales/intl/en/timepiece/product/watch/M/MT/MTP/mtp-m305d-1a2v/assets/MTP-M305D-1A2V.png.transform/product-panel/image.png" alt="" />
+                    <div className="pl-4">
+                        <span>PRODUCT!</span>
+                        <p className='text-red-500'>$29</p>
+                    </div>
+                    <div className=" pl-16 items-center pb-2">
+                        <button className='flex items-center justify-center h-8 bg-yellow-200 w-32 rounded-md'>Add To Card</button>
+
+                    </div>
+
+                </div>
+                <div className=" w-64 bg-slate-950 rounded-md shadow-sm" >
+                    <img src="https://i.pinimg.com/originals/cd/57/8f/cd578f38e2878c63f9402ed15b3484cc.jpg" alt="" />
+                    <div className="pl-4">
+                        <span>PRODUCT!</span>
+                        <p className='text-red-500'>$29</p>
+                    </div>
+                    <div className=" pl-16 items-center pb-2">
+                        <button className='flex items-center justify-center h-8 bg-yellow-200 w-32 rounded-md'>Add To Card</button>
+
+                    </div>
+
+                </div>
+                <div className=" w-64 bg-slate-950 rounded-md shadow-sm" >
+                    <img src="https://i.pinimg.com/originals/cd/57/8f/cd578f38e2878c63f9402ed15b3484cc.jpg" alt="" />
+                    <div className="pl-4">
+                        <span>PRODUCT!</span>
+                        <p className='text-red-500'>$29</p>
+                    </div>
+                    <div className=" pl-16 items-center pb-2">
+                        <button className='flex items-center justify-center h-8 bg-yellow-200 w-32 rounded-md'>Add To Card</button>
+
+                    </div>
+
+                </div>
+                <div className=" w-64 bg-slate-950 rounded-md shadow-sm" >
+                    <img src="https://i.pinimg.com/originals/cd/57/8f/cd578f38e2878c63f9402ed15b3484cc.jpg" alt="" />
+                    <div className="pl-4">
+                        <span>PRODUCT!</span>
+                        <p className='text-red-500'>$29</p>
+                    </div>
+                    <div className=" pl-16 items-center pb-2">
+                        <button className='flex items-center justify-center h-8 bg-yellow-200 w-32 rounded-md'>Add To Card</button>
+
+                    </div>
+
+                </div>
+                <div className=" w-64 bg-slate-950 rounded-md shadow-sm" >
+                    <img src="https://i.pinimg.com/originals/cd/57/8f/cd578f38e2878c63f9402ed15b3484cc.jpg" alt="" />
+                    <div className="pl-4">
+                        <span>PRODUCT!</span>
+                        <p className='text-red-500'>$29</p>
+                    </div>
+                    <div className=" pl-16 items-center pb-2">
+                        <button className='flex items-center justify-center h-8 bg-yellow-200 w-32 rounded-md'>Add To Card</button>
+
+                    </div>
+
+                </div>
+
+                <div className=" w-64 bg-slate-950 rounded-md shadow-sm" >
+                    <img src="https://www.casio.com/content/dam/casio/product-info/locales/intl/en/timepiece/product/watch/M/MT/MTP/mtp-m305d-2av/assets/MTP-M305D-2AV.png.transform/main-visual-pc/image.png" alt="" />
+                    <div className="pl-4">
+                        <span>PRODUCT!</span>
+                        <p className='text-red-500'>$29</p>
+                    </div>
+                    <div className=" pl-16 items-center pb-2">
+                        <button className='flex items-center justify-center h-8 bg-yellow-200 w-32 rounded-md'>Add To Card</button>
+
+                    </div>
+
+                </div>
+                <div className=" w-64 bg-slate-950 rounded-md shadow-sm" >
+                    <img src="https://i.pinimg.com/originals/cd/57/8f/cd578f38e2878c63f9402ed15b3484cc.jpg" alt="" />
+                    <div className="pl-4">
+                        <span>PRODUCT!</span>
+                        <p className='text-red-500'>$29</p>
+                    </div>
+                    <div className=" pl-16 items-center pb-2">
+                        <button className='flex items-center justify-center h-8 bg-yellow-200 w-32 rounded-md'>Add To Card</button>
+
+                    </div>
+
+                </div>
+                <div className=" w-64 bg-slate-950 rounded-md shadow-sm" >
+                    <img src="https://i.pinimg.com/originals/cd/57/8f/cd578f38e2878c63f9402ed15b3484cc.jpg" alt="" />
+                    <div className="pl-4">
+                        <span>PRODUCT!</span>
+                        <p className='text-red-500'>$29</p>
+                    </div>
+                    <div className=" pl-16 items-center pb-2">
+                        <button className='flex items-center justify-center h-8 bg-yellow-200 w-32 rounded-md'>Add To Card</button>
+
+                    </div>
+
+                </div>
+                <div className=" w-64 bg-slate-950 rounded-md shadow-sm" >
+                    <img src="https://i.pinimg.com/originals/cd/57/8f/cd578f38e2878c63f9402ed15b3484cc.jpg" alt="" />
+                    <div className="pl-4">
+                        <span>PRODUCT!</span>
+                        <p className='text-red-500'>$29</p>
+                    </div>
+                    <div className=" pl-16 items-center pb-2">
+                        <button className='flex items-center justify-center h-8 bg-yellow-200 w-32 rounded-md'>Add To Card</button>
+
+                    </div>
+
+                </div>
+                <div className=" w-64 bg-slate-950 rounded-md shadow-sm" >
+                    <img src="https://i.pinimg.com/originals/cd/57/8f/cd578f38e2878c63f9402ed15b3484cc.jpg" alt="" />
+                    <div className="pl-4">
+                        <span>PRODUCT!</span>
+                        <p className='text-red-500'>$29</p>
+                    </div>
+                    <div className=" pl-16 items-center pb-2">
+                        <button className='flex items-center justify-center h-8 bg-yellow-200 w-32 rounded-md'>Add To Card</button>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+
+
+
+
+
+
+
+
+
+
+            <div className="pr-24  w-full row-span-2 text-white  ">
+                <div className=" w-96  bg-slate-950 rounded-md shadow-sm" >
+                    <img src="https://i.pinimg.com/originals/cd/57/8f/cd578f38e2878c63f9402ed15b3484cc.jpg" alt="" />
+                    <div className="pl-4">
+                        <span>PRODUCT!</span>
+                        <p className='text-red-500'>$29</p>
+                    </div>
+                    <div className=" pl-16 items-center pb-2">
+                        <button className='flex items-center justify-center h-8 bg-yellow-200 w-32 rounded-md'>Add To Card</button>
+
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+    </>
+
+
+
+
+    )
+}
+
+export default Home
