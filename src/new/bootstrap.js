9e825fa4a7ac44db83de3ab1ab7c5bd28fc31628
VKongkin <voeun_kongkin@pp.bbu.edu.kh>
2024-10-12 19:31:07 +0700

    package: Install bootstrap

---
+++ src/new/bootstrap.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col, Jumbotron, Button } from 'react-bootstrap';
+
+const WelcomePage = () => {
+    return (
+        <Container>
+            <Jumbotron className="mt-5">
+                <h1>Welcome to Our Website!</h1>
+                <p>
+                    This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.
+                </p>
+                <p>
+                    <Button variant="primary">Learn more</Button>
+                </p>
+            </Jumbotron>
+        </Container>
+    );
+};
+
+export default WelcomePage;
